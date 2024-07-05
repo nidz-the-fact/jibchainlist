4631df10b8c893d26f125a74e289b1474a388ef2
Nidz The Fact <106298826+nidz-the-fact@users.noreply.github.com>
2024-07-05 18:25:06 +0700

    Update chains.js

--- components/chains.js
+++ components/chains.js
@@ -1,18 +1,4 @@
 export const chainIds = {
-  56: "bnb",
-  73: "fncy",
-  97: "bnb",
-  117: "bas-ars",
-  230: "bas-gal",
-  923018: "fncy",
-  14000: "bas-ankr",
-  91715: "combo",
-  9980: "combo",
-  5600: "bnb",
-  5601: "bnb",
-  5611: "bnb",
-  204: "bnb",
-  1017: "bnb",
-
+  7003: "pom",
   7004: "sense",
 };
